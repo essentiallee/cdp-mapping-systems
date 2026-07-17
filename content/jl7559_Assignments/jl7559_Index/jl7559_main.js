0cfcbc6b3cf662b2d31ee9e4a5ae3963d899de55
Jae Hyun Lee <jhlee95424@gmail.com>
2026-07-17 12:59:13 -0400

    interactivity added

---
+++ content/jl7559_Assignments/jl7559_Index/jl7559_main.js
@@ -0,0 +1,53 @@
+  var map = new maplibregl.Map({
+        container: 'map', // container id
+        style: 'style@440_theme@light_lang@en.json', // style URL
+        zoom: 6 // starting zoom
+    });
+
+map.addControl(new maplibregl.NavigationControl());
+
+map.on("load", () => {
+  fetch("https://data.cityofnewyork.us/resource/43nn-pn8j.geojson?cuisine_description=Pizza")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      data.features.forEach((feature) => {
+        feature.geometry = {
+          type: "Point",
+          coordinates: [
+            Number(feature.properties.longitude),
+            Number(feature.properties.latitude)
+          ]
+        };
+      });
+
+      map.addSource("restaurants", {
+        type: "geojson",
+        data: data
+      });
+
+      map.addLayer({
+        id: "restaurants-layer",
+        type: "circle",
+        source: "restaurants",
+        paint: {
+          "circle-radius": 5,
+          "circle-color": "#111"
+        }
+      });
+
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+});
+
+    map.on("click", "restaurants-layer", (e) => {
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const description = e.features[0].properties.dba;
+      new maplibregl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(description)
+        .addTo(map);
+    });w
