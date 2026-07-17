7ae5ca719013bf0d0e5fde040e21835f9b68877c
Jae Hyun Lee <jhlee95424@gmail.com>
2026-07-17 11:25:48 -0400

    maplibre

---
+++ content/jl7559_Assignments/jl7559_Javascript/jl7559_main.js
@@ -0,0 +1,6 @@
+  var map = new maplibregl.Map({
+        container: 'map', // container id
+        style: 'https://demotiles.maplibre.org/style.json', // style URL for basemap
+        center: [-73.97144, 40.70491], // starting position [lng, lat]
+        zoom: 6 // starting zoom
+    });
